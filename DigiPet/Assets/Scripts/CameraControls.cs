4f503ff3b935d5128dd238dc8fac302786e45c20
rrrobi <rrrrobi@msn.com>
2018-04-02 22:38:01 +0100

    Camera control initial Setup
    Camera control Forward-Back, Left-Right. boundry limits in place. Usable only from mobile device ATM

--- DigiPet/Assets/Scripts/CameraControls.cs
+++ DigiPet/Assets/Scripts/CameraControls.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class CameraControls : MonoBehaviour {
 
-    public float PanSpeed = 0.1f;
+    public float PanSpeed = 0.05f;
 
     // Use this for initialization
     void Start () {
@@ -16,14 +16,28 @@ public class CameraControls : MonoBehaviour {
         ControlCamera();
 	}
 
+
     private void ControlCamera()
     {
+        ////
+        //  Standard camera movement moves left-right, Forward-back using world space
+        ////
         if ((Input.touchCount > 1 && Input.touchCount < 3) &&
             Input.GetTouch(0).phase == TouchPhase.Moved)
         {
             Vector2 touchDeltaPosition = Input.GetTouch(0).deltaPosition;
-            transform.Translate(-touchDeltaPosition.x * PanSpeed, -touchDeltaPosition.y * PanSpeed, 0); // control is for x z axis
+            Debug.Log("touch Delta Pos: " + touchDeltaPosition);
+            transform.Translate(-touchDeltaPosition.y * PanSpeed, 0, touchDeltaPosition.x * PanSpeed, Space.World); // control is for x z axis
+
+            transform.position = new Vector3(Mathf.Clamp(transform.position.x, -28.0f, 16.0f), Mathf.Clamp(transform.position.y, 1.25f, 8.0f), Mathf.Clamp(transform.position.z, -40.0f, 2.5f));
         }
+        // Todo: Mouse drag camera controls (for debuging without mobile device)
+
+        //// TODO:
+        //  Cinematic mode (uses local space)
+        //  Locks camera to pet, swipe left and right will rotate around point centered on pet.
+        //  swiping up-down zooms in and out (within reason)
+        ////
 
         if (Input.GetKey(KeyCode.DownArrow))
         {
